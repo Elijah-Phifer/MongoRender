c5dae39dc400356e9fd466f94eb938a669dc3d6a
Elijah-Phifer <ephifer21@gmail.com>
2024-03-05 00:56:25 -0600

    newest stuff

--- testmongo.js
+++ testmongo.js
@@ -1,7 +1,9 @@
 const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://<user>:<password>@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://e:vokzyh-2Muqxi-kopqed@cluster0.twsoe6v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+//"mongodb+srv://<user>:<password>@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
 
 // --- This is the standard stuff to get it to work on the browser
 const express = require('express');
@@ -33,8 +35,8 @@ console.log("Looking for: " + searchKey);
 
 async function run() {
   try {
-    const database = client.db('ckmdb');
-    const parts = database.collection('cmps415');
+    const database = client.db('Cluster0');
+    const parts = database.collection('ElijahDatabase.ElijahCollection');
 
     // Hardwired Query for a part that has partID '12345'
     // const query = { partID: '12345' };
